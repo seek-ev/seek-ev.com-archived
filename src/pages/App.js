a560349930f152cd440981814d78e744e899e35b
SerekKiri <kacper.potyrala@gmail.com>
2020-10-07 13:41:37 +0200

    style(component): display example buttons

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,8 +3,42 @@ import React from 'react'
 // Styles
 import './App.sass'
 
+// Components
+import Button from '../components/basic/button'
+
 function App() {
-  return <div className='app'></div>
+  return (
+    <div className='app'>
+      <div class='example'>
+        <div class='example_text'>Primary</div>
+        <Button text='Log in' />
+      </div>
+      <div class='example'>
+        <div class='example_text'>Clean</div>
+        <Button text='Log in' clean />
+      </div>
+      <div class='example'>
+        <div class='example_text'>Raise</div>
+        <Button text='Log in' raise />
+      </div>
+      <div class='example'>
+        <div class='example_text'>Error</div>
+        <Button text='Log in' error />
+      </div>
+      <div class='example'>
+        <div class='example_text'>Dark</div>
+        <Button text='Log in' dark />
+      </div>
+      <div class='example'>
+        <div class='example_text'>Blue</div>
+        <Button text='Log in' blue />
+      </div>
+      <div class='example'>
+        <div class='example_text'>Round</div>
+        <Button text='Log in' round />
+      </div>
+    </div>
+  )
 }
 
 export default App
