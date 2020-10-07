a45185d3e337c7fc8f75ac2ad9dfea4697690729
SerekKiri <kacper.potyrala@gmail.com>
2020-10-07 18:29:45 +0200

    style(component): brand new inputs

---
+++ src/components/Basic/input/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+// Styles
+import './input.sass'
+
+const Input = (props) => {
+  function handleChange(e) {
+    // onChange prop is required
+    props.onChange(e.target)
+  }
+
+  return (
+    <div className='s-input'>
+      <input
+        value={props.value}
+        onChange={handleChange}
+        className={
+          (props.raise ? 'raise-input ' : '') +
+          (props.border ? 'border-input ' : '') +
+          (props.error ? 'error-input ' : '') +
+          (props.valid ? 'valid-input ' : '') +
+          (props.round ? 'round-input ' : '')
+        }
+        name={props.name}
+        type={props.type}
+        placeholder={props.placeholder}
+        required={props.required}
+      />
+
+      <div className='error-message'>{props.error}</div>
+    </div>
+  )
+}
+
+export default Input
