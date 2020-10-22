2ffc77826de7d6a50d8f0a77eb104ebf1867456d
SerekKiri <kacper.potyrala@gmail.com>
2020-10-22 23:08:17 +0200

    feat: Navbar component

---
+++ src/components/navbar/nav-profile/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// Styles
+import './nav_profile.sass'
+
+const NavProfile = (props) => {
+    return (
+        <Link to='/me' className='nav-profile'>
+            <div className="nav-profile-avatar">
+                <img src="cat.png" alt="avatar" />
+            </div>
+
+            <div className='nav-profile-username'>
+                {props.username}
+            </div>
+        </Link>
+    )
+}
+
+export { NavProfile }
