730baba55140e50320f560ac6a265365606bcf82
SerekKiri <kacper.potyrala@gmail.com>
2020-11-04 19:22:13 +0100

    feat: fetch new car if params changed

--- src/pages/Car/index.js
+++ src/pages/Car/index.js
@@ -8,6 +8,7 @@ import './car.sass'
 // Components
 import { Navbar } from '../../components/navbar'
 import { CarInfo } from '../../components/car/car-info'
+import { CarMenu } from '../../components/car/car-menu'
 import { CarDetails } from '../../components/car/car-details'
 import { CarPictures } from '../../components/car/car-pictures'
 
@@ -21,17 +22,33 @@ class Car extends React.Component {
 
         this.state = {
             car: {},
+            currentCar: '',
             loading: true
         }
     }
 
     async componentDidMount() {
-        await axios.get(`/cars/model/${this.props.location.pathname.replace('/', '')}`).then(res => this.setState({ car: res.data }))
+        await axios.get(`/cars/model/${this.props.match.params.model}`).then(res => this.setState({ car: res.data, currentCar: res.data.model }))
             .catch(err => this.props.showSnackbar(err, 'error'))
 
         if (this.state.car) document.title = this.state.car.model
 
+        this._ismounted = true
+
         this.setState({ loading: false })
+    }
+
+    async componentDidUpdate() {
+        if ((this._ismounted && !this.state.loading) && (this.props.match.params.model.toLowerCase() !== this.state.currentCar.toLowerCase())) {
+            this.setState({ loading: true })
+
+            await axios.get(`/cars/model/${this.props.match.params.model}`).then(res => this.setState({ car: res.data, currentCar: res.data.model }))
+                .catch(err => this.props.showSnackbar(err, 'error'))
+
+            if (this.state.car) document.title = this.state.car.model
+
+            this.setState({ loading: false })
+        }
     }
 
     componentWillUnmount() {
@@ -44,8 +61,11 @@ class Car extends React.Component {
                 <Navbar />
                 <div className={Object.keys(this.state.car).length > 0 && !this.state.loading ? 'car-landing' : 'car-hidden'}>
                     <div className="car-header">
-                        <CarPictures pictures={this.state.car.pictures} />
-                        <CarInfo model={this.state.car.model} productionYears={this.state.car.productionYears} category={this.state.car.category} brand={this.state.car.brand} createdBy={this.state.car.admin ? this.state.car.admin : this.state.car.user} />
+                        <div className="car-header-info">
+                            <CarPictures pictures={this.state.car.pictures} />
+                            <CarInfo model={this.state.car.model} productionYears={this.state.car.productionYears} category={this.state.car.category} brand={this.state.car.brand} createdBy={this.state.car.admin ? this.state.car.admin : this.state.car.user} />
+                        </div>
+                        <CarMenu versions={this.state.car.versions} />
                     </div>
                     <div className="car-details-container">
                         <CarDetails />
