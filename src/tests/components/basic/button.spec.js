2690c017fb2b3283f5aa666cb0858e948a4689fb
Kiri <kacper.potyrala@gmail.com>
2020-12-21 22:52:33 +0100

    test: input component

--- src/tests/components/basic/button.spec.js
+++ src/tests/components/basic/button.spec.js
@@ -5,19 +5,18 @@ import { shallow } from 'enzyme'
 import { Button } from '../../../components/basic/button'
 
 describe('Button Component', () => {
-    it('Set button class correctly', () => {
+    it('Render button with proper class', () => {
         const button = shallow(<Button />)
         expect(button.find('.btn')).toHaveLength(1)
     })
 
-    it('Displays text correctly', () => {
+    it('Displays text', () => {
         const button = shallow(<Button text="text" />)
         expect(button.text()).toEqual('text')
     })
 
     it('Click event', () => {
         const mockCallback = jest.fn()
-
         const button = shallow(<Button onClick={mockCallback} />)
         button.find('button').simulate('click')
         expect(mockCallback.mock.calls.length).toEqual(1)
@@ -25,7 +24,6 @@ describe('Button Component', () => {
 
     it('Disabled Props', () => {
         const mockCallback = jest.fn()
-
         const button = shallow(<Button onClick={mockCallback} />)
         button.setProps({ disabled: true })
         expect(mockCallback.mock.calls.length).toEqual(0)
@@ -33,43 +31,36 @@ describe('Button Component', () => {
 
     it('Primary props', () => {
         const button = shallow(<Button primary />)
-
         expect(button.find('.primary-button')).toHaveLength(1)
     })
 
     it('Error props', () => {
         const button = shallow(<Button error />)
-
         expect(button.find('.error-button')).toHaveLength(1)
     })
 
     it('Raise props', () => {
         const button = shallow(<Button raise />)
-
         expect(button.find('.raise-button')).toHaveLength(1)
     })
 
     it('Blue props', () => {
         const button = shallow(<Button blue />)
-
         expect(button.find('.blue-button')).toHaveLength(1)
     })
 
     it('Dark props', () => {
         const button = shallow(<Button dark />)
-
         expect(button.find('.dark-button')).toHaveLength(1)
     })
 
     it('Clean props', () => {
         const button = shallow(<Button clean />)
-
         expect(button.find('.clean-button')).toHaveLength(1)
     })
 
     it('Round props', () => {
         const button = shallow(<Button round />)
-
         expect(button.find('.round-button')).toHaveLength(1)
     })
 })
