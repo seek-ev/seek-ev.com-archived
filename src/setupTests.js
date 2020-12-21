75a6787894c183b8d8ac7d80cd13dbe9168bf960
Kiri <kacper.potyrala@gmail.com>
2020-12-21 15:03:24 +0100

    chore: changed enzyme adapter version to correct one

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,6 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect'
 import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16.3'
+import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
