ab33f55216ab5d4530a43ea1bc2c270f8ba604b1
Kiri <kacper.potyrala@gmail.com>
2021-01-02 22:23:36 +0100

    refactor: snackbar component

--- src/components/notifications/snackbar/index.jsx
+++ src/components/notifications/snackbar/index.jsx
@@ -3,29 +3,25 @@ import { useSelector, useDispatch } from 'react-redux'
 import { hideSnackbar } from 'actions/snackbar'
 
 // Styles
-import './snackbar.sass'
+import { Wrapper, SnackText, SnackButton } from './styles'
 
 const Snackbar = () => {
     const snackState = useSelector(state => state.snackbar)
     const dispatch = useDispatch()
 
     return (
-        <div className={'snackbar' +
-            (snackState.show ? ' show-snack' : ' hide-snack') +
-            (snackState.type === 'success' ? ' snack-success' : snackState.type === 'error' ? ' snack-error' : ' snack-basic')
-        }
-        >
-            <div className="snack-text">
+        <Wrapper show={snackState.show} type={snackState.type} >
+            <SnackText>
                 {snackState.text.response ? snackState.text.response.status === 429 ?
                     'Too many requests, please try again later.' : snackState.text.response.data ?
                         snackState.text.response.data.error ? snackState.text.response.data.error : snackState.text.response.data.message ? snackState.text.response.data.message
                             : snackState.text.toString() : snackState.text.response.statusText ? snackState.text.response.statusText : snackState.text : snackState.text.message ? snackState.text.message : snackState.text}
-            </div>
+            </SnackText>
 
-            <button className="snack-button" onClick={() => dispatch(hideSnackbar())}>
+            <SnackButton onClick={() => dispatch(hideSnackbar())}>
                 Close
-            </button>
-        </div>
+            </SnackButton>
+        </Wrapper>
     )
 }
 
---
+++ src/components/notifications/snackbar/styles.js
@@ -0,0 +1,111 @@
+import styled from 'styled-components/macro'
+
+// Variables
+import { colors } from 'assets/variables'
+
+const Wrapper = styled.div`
+    left: 50%;
+    z-index: 1;
+    color: #fff;
+    bottom: 10px;
+    display: flex;
+    transition: 0.5s;
+    position: fixed;
+    text-align: left;
+    min-width: 250px;
+    max-width: 400px;
+    border-radius: 7px;
+    visibility: hidden;
+    transform: translate(-50%);
+    flex-direction: row;
+    padding: 14px 7px 14px 12px;
+    justify-content: space-between;
+    font-family: 'Roboto', sans-serif;
+
+    ${props => props.show ? `
+        z-index: 1;
+        position: fixed;
+        visibility: visible;
+        animation: fadein 0.5s;
+        -webkit-animation: fadein 0.5s;
+    ` :
+        `
+        animation: fadeout 0.5s;
+        -webkit-animation: fadeout 0.5s;
+    `
+    }
+
+    ${props => props.type === 'success' ? `background-color: ${colors.primary};` :
+        props.type === 'error' ? `background-color: ${colors.error};`
+            : 'background-color: #333;'} 
+
+    @-webkit-keyframes fadein {
+        0% {
+            bottom: 0;
+            opacity: 0;
+        }
+
+        100% {
+            bottom: 10px;
+            opacity: 1;
+        }
+    }
+
+    @keyframes fadein {
+        0% {
+            bottom: 0;
+            opacity: 0;
+        }
+
+        100% {
+            bottom: 10px;
+            opacity: 1;
+        }
+    }
+
+    @-webkit-keyframes fadeout {
+        0% {
+            bottom: 10px;
+            opacity: 1;
+        }
+
+        100% {
+            bottom: 0;
+            opacity: 0;
+        }
+    }
+
+    @keyframes fadeout {
+        0% {
+            bottom: 10px;
+            opacity: 1;
+        }
+
+        100% {
+            bottom: 0;
+            opacity: 0;
+        }
+    }
+`
+
+const SnackText = styled.div`
+    max-width: 300px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+const SnackButton = styled.button`
+    width: 60px;
+    border: none;
+    outline: none;
+    color: #e9d520;
+    font-size: 14px;
+    background: none;
+    font-weight: 500;
+    margin: 0 0 0 10px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-family: 'Roboto', sans-serif;
+`
+
+export { Wrapper, SnackText, SnackButton }
