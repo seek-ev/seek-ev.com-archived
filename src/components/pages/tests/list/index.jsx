13ce87d76126228067681e6d154aecd4cf728646
SerekKiri <kacper.potyrala@gmail.com>
2021-02-24 10:39:44 +0100

    fix: pagination

--- src/components/pages/tests/list/index.jsx
+++ src/components/pages/tests/list/index.jsx
@@ -9,23 +9,17 @@ import { Test } from './test'
 const TestsList = ({ tests }) => {
     const [pages, setPages] = useState(1)
     const [list, setList] = useState([])
-    const [page, setPage] = useState(1)
+    const [page, setPage] = useState(0)
 
     useEffect(() => {
-        const testArr = []
-        let x = 0
-        while (x < tests.length && x <= 10) {
-            testArr.push(tests[x])
-            x++
-        }
-        setList(testArr)
+        setList(tests.length > 0 ? tests.slice(0, 10) : [])
         setPages(Math.ceil(tests.length / 10))
     }, [tests])
 
     const changePage = (newPage) => {
         if (newPage === page) return
-        if (newPage === 1) setList(tests.slice(0, 10))
-        else setList(tests.slice(page * 10, page * 10 + 10))
+        if (newPage === 0) setList(tests.slice(0, 10))
+        else setList(tests.slice(newPage * 10, newPage * 10 + 10))
         setPage(newPage)
     }
 
@@ -34,7 +28,7 @@ const TestsList = ({ tests }) => {
             {list.map((t, i) => { return <Test test={t} key={t.id} /> })}
             <Pagination>
                 {Array.from(Array(pages), (e, i) => {
-                    return <Page chosen={page === (i + 1)} key={i} onClick={() => changePage(i + 1)}>{i + 1}</Page>
+                    return <Page chosen={page === (i)} key={i} onClick={() => changePage(i)}>{i + 1}</Page>
                 })}
             </Pagination>
         </Wrapper>
