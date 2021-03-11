96c9ec328ae5209ced6def773440ca48f0997445
SerekKiri <kacper.potyrala@gmail.com>
2021-03-11 09:25:51 +0100

    feat: new range test form

--- src/components/pages/tests-new/steps/test/form/types/range/index.jsx
+++ src/components/pages/tests-new/steps/test/form/types/range/index.jsx
@@ -4,8 +4,11 @@ import React, { useState, useEffect } from 'react'
 import { Wrapper, TestInput } from '../forms'
 
 const Range = ({ submit, setSubmit, create, loading }) => {
-    const [range, setRange] = useState({ velocity: {} })
+    const [charging, setCharging] = useState({})
     const [errors, setError] = useState({})
+    const [range, setRange] = useState({})
+
+
 
     useEffect(() => {
         // If form was not submitted don't validate
@@ -14,8 +17,17 @@ const Range = ({ submit, setSubmit, create, loading }) => {
         // Errors
         const valid = {}
 
+        if (!charging.time) valid.time = 'Field required'
+
         // Check if any value is missing
         if (!range.season) valid.season = 'Field required'
+        if (!range.surface) valid.surface = 'Field required'
+        if (!range.tires) valid.tires = 'Field required'
+        if (!range.averageSpeed) valid.averageSpeed = 'Field required'
+        if (!range.batteryCapacity) valid.batteryCapacity = 'Field required'
+        if (!range.km) valid.km = 'Field required'
+        if (!range.mi) valid.mi = 'Field required'
+
         // Set errors if any occurs
         setError({ ...errors, ...valid })
 
@@ -26,29 +38,34 @@ const Range = ({ submit, setSubmit, create, loading }) => {
         if (Object.keys(valid).length || Object.keys(errors).length > 0) return
 
         // Pass data to parent
-        //create(range)
+        create(range)
     }, [submit, range, create, errors, setError, setSubmit])
 
     const validate = async (name, type, value) => {
-        if (type === 'text') return
-        if (value < 0) return setError({ ...errors, [name]: 'It has to be a positive number' })
-        else {
+        if (type !== 'number' || value >= 0) {
             const errClone = { ...errors }
             delete errClone[name]
             return setError(errClone)
         }
+        else return setError({ ...errors, [name]: 'It has to be a positive number' })
     }
 
     const setProperty = (e) => {
         validate(e.name, e.type, e.value)
-        setRange({ ...range, [e.name]: e.type === 'number' ? parseInt(e.value) : e.value })
+        return setRange({ ...range, [e.name]: e.type === 'number' ? parseInt(e.value) : e.value })
+    }
+
+    const setChargingProperty = (e) => {
+        validate(e.name, e.type, e.value)
+        setCharging({ ...charging, [e.name]: parseInt(e.value) })
+        return setRange({ ...range, chargingTo75Percent: { ...charging, [e.name]: parseInt(e.value) } })
     }
 
     return (
         <Wrapper>
             <TestInput title="Season" name="season" placeholder="Season" value={range.season || ''} onChange={setProperty} type="text" error={errors.season} disabled={loading} />
             <TestInput title="Surface" name="surface" placeholder="Surface" value={range.surface || ''} onChange={setProperty} type="text" error={errors.surface} disabled={loading} />
-            <TestInput title="Temperature" name="temperature" placeholder="Temperature" value={range.temperature || ''} onChange={setProperty} type="numer" step="1" min="-50" error={errors.temperature} disabled={loading} />
+            <TestInput title="Temperature" name="temperature" placeholder="Temperature" value={range.temperature || ''} onChange={setProperty} type="number" step="1" min="-50" max="70" error={errors.temperature} disabled={loading} />
             <TestInput title="Tires" name="tires" placeholder="Tires" value={range.tires || ''} onChange={setProperty} type="text" error={errors.tires} disabled={loading} />
             <TestInput title="Wheel front" name="wheelFront" placeholder="Wheel front" value={range.wheelFront || ''} onChange={setProperty} type="text" error={errors.wheelFront} disabled={loading} />
             <TestInput title="Wheel rear" name="wheelRear" placeholder="Wheel rear" value={range.wheelRear || ''} onChange={setProperty} type="text" error={errors.wheelRear} disabled={loading} />
@@ -58,6 +75,8 @@ const Range = ({ submit, setSubmit, create, loading }) => {
             <TestInput title="Kilometers" name="km" placeholder="Kilometers" value={range.km || ''} onChange={setProperty} type="number" step="1" min="0" max="1000" error={errors.km} disabled={loading} />
             <TestInput title="Miles" name="mi" placeholder="Miles" value={range.mi || ''} onChange={setProperty} type="number" step="1" min="0" max="1000" error={errors.mi} disabled={loading} />
             <TestInput title="Range with 75%" name="rangeWith75Percent" placeholder="Range with 75%" value={range.rangeWith75Percent || ''} onChange={setProperty} type="number" step="1" min="0" max="1000" error={errors.rangeWith75Percent} disabled={loading} />
+            <TestInput title="Charging time to 75%" name="time" placeholder="Charging time to 75%" value={charging.time || ''} onChange={setChargingProperty} type="number" step="1" min="0" max="1000" error={errors.time} disabled={loading} />
+            <TestInput title="Charging speed in kmph" name="kmPerHour" placeholder="Charging speed" value={charging.kmPerHour || ''} onChange={setChargingProperty} type="number" step="1" min="0" max="1000" error={errors.kmPerHour} disabled={loading} />
         </Wrapper>
     )
 }
