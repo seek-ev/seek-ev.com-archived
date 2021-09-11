0f9c31b0aff934cc6e84468870b9d0993a667b4f
SerekKiri <kacper.potyrala@gmail.com>
2021-09-11 18:21:57 +0200

    loading is now a separate reusable component

---
+++ src/components/basic/loading/styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+
+// Variables
+import { colors } from 'assets/variables'
+
+const Loading = styled.div`
+    background: ${colors.primary};
+    -webkit-animation: load1 1s infinite ease-in-out;
+    animation: load1 1s infinite ease-in-out;
+    width: 7px;
+    height: 20px;
+    color: ${colors.primary};
+    text-indent: -9999em;
+    margin: 88px auto;
+    position: relative;
+    font-size: 8px;
+    top: 100px;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+
+    &:before,
+    &:after {
+        background: ${colors.primary};
+        -webkit-animation: load1 1s infinite ease-in-out;
+        animation: load1 1s infinite ease-in-out;
+        width: 7px;
+        height: 20px;
+    }
+
+    &:before,
+    &:after {
+        position: absolute;
+        top: 0;
+        content: '';
+    }
+
+    &:before {
+        left: -1.5em;
+        -webkit-animation-delay: -0.32s;
+        animation-delay: -0.32s;
+    }
+    &:after {
+        left: 1.5em;
+    }
+
+    @-webkit-keyframes load1 {
+        0%, 80%, 100% {
+            box-shadow: 0 0;
+            height: 4em;
+        }
+        40% {
+            box-shadow: 0 -2em;
+            height: 5em;
+        }
+    }
+
+    @keyframes load1 {
+        0%, 80%, 100% {
+            box-shadow: 0 0;
+            height: 4em;
+        }
+        40% {
+            box-shadow: 0 -2em;
+            height: 5em;
+        }
+    }
+`
+
+export { Loading }
